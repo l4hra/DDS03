531f25a58031aab5c5a8c640dd18b2b429be328d
Lahra Nunes <135645793+l4hra@users.noreply.github.com>
2024-09-03 18:06:08 -0300

    subindo novo painel

--- 09_02/table.js
+++ 09_02/table.js
@@ -1,74 +1,252 @@
-'use strict';
-
-
+"use strict";
 
 //DOM (Document Objetc Model) é a estrutura da página HTML.
 //O DOM permite que linguagens interajam com a página
 
-
 let aulas = [
-    {
-        "id": 1300,
-        "data": "2024-08-29T03:00:00.000Z",
-        "data_hora_inicio": "2024-08-29T21:00:00.000Z",
-        "data_hora_fim": "2024-08-30T01:00:00.000Z",
-        "turma": "EMP-NBM-03",
-        "instrutor": "JOEL HERBERT BARBOSA SILVA",
-        "unidade_curricular": "NOÇÕES BÁSICAS PARA MAQUINISTAS (CH: 219.0000)",
-        "ambiente": "VTRIA-3-SALA-3004",
-        "chave": null
-    },
-    {
-        "id": 1280,
-        "data": "2024-08-29T03:00:00.000Z",
-        "data_hora_inicio": "2024-08-29T21:00:00.000Z",
-        "data_hora_fim": "2024-08-30T01:00:00.000Z",
-        "turma": "UMO-MBMM-03",
-        "instrutor": "THADEU VASCONCELOS DA SILVA GOMES",
-        "unidade_curricular": "MECÂNICA BÁSICA DE MOTORES DE MOTOCICLETAS (CH: 100.0000)",
-        "ambiente": "VTRIA-EXTER-EXTERNO",
-        "chave": null
-    },
-    {
-        "id": 1326,
-        "data": "2024-08-29T03:00:00.000Z",
-        "data_hora_inicio": "2024-08-29T21:30:00.000Z",
-        "data_hora_fim": "2024-08-30T01:00:00.000Z",
-        "turma": "HTC-MEC-4-92",
-        "instrutor": "AFONSO DE JESUS CORDEIRO",
-        "unidade_curricular": "DESENVOLVIMENTO DE SISTEMAS DE AUTOMAÇÃO MECÂNICA (CH: 100.0000)",
-        "ambiente": "VTRIA-3-LAB-3003",
-        "chave": null
-    }
-]
+  {
+    id: 1300,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:00:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "EMP-NBM-03",
+    instrutor: "JOEL HERBERT BARBOSA SILVA",
+    unidade_curricular: "NOÇÕES BÁSICAS PARA MAQUINISTAS (CH: 219.0000)",
+    ambiente: "VTRIA-3-SALA-3004",
+    chave: null,
+  },
+  {
+    id: 1280,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:00:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "UMO-MBMM-03",
+    instrutor: "THADEU VASCONCELOS DA SILVA GOMES",
+    unidade_curricular:
+      "MECÂNICA BÁSICA DE MOTORES DE MOTOCICLETAS (CH: 100.0000)",
+    ambiente: "VTRIA-EXTER-EXTERNO",
+    chave: null,
+  },
+  {
+    id: 1326,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:30:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "HTC-MEC-4-92",
+    instrutor: "AFONSO DE JESUS CORDEIRO",
+    unidade_curricular:
+      "DESENVOLVIMENTO DE SISTEMAS DE AUTOMAÇÃO MECÂNICA (CH: 100.0000)",
+    ambiente: "VTRIA-3-LAB-3003",
+    chave: null,
+  },
+  {
+    id: 1300,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:00:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "EMP-NBM-03",
+    instrutor: "JOEL HERBERT BARBOSA SILVA",
+    unidade_curricular: "NOÇÕES BÁSICAS PARA MAQUINISTAS (CH: 219.0000)",
+    ambiente: "VTRIA-3-SALA-3004",
+    chave: null,
+  },
+  {
+    id: 1280,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:00:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "UMO-MBMM-03",
+    instrutor: "THADEU VASCONCELOS DA SILVA GOMES",
+    unidade_curricular:
+      "MECÂNICA BÁSICA DE MOTORES DE MOTOCICLETAS (CH: 100.0000)",
+    ambiente: "VTRIA-EXTER-EXTERNO",
+    chave: null,
+  },
+  {
+    id: 1326,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:30:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "HTC-MEC-4-92",
+    instrutor: "AFONSO DE JESUS CORDEIRO",
+    unidade_curricular:
+      "DESENVOLVIMENTO DE SISTEMAS DE AUTOMAÇÃO MECÂNICA (CH: 100.0000)",
+    ambiente: "VTRIA-3-LAB-3003",
+    chave: null,
+  },
+  {
+    id: 1300,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:00:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "EMP-NBM-03",
+    instrutor: "JOEL HERBERT BARBOSA SILVA",
+    unidade_curricular: "NOÇÕES BÁSICAS PARA MAQUINISTAS (CH: 219.0000)",
+    ambiente: "VTRIA-3-SALA-3004",
+    chave: null,
+  },
+  {
+    id: 1280,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:00:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "UMO-MBMM-03",
+    instrutor: "THADEU VASCONCELOS DA SILVA GOMES",
+    unidade_curricular:
+      "MECÂNICA BÁSICA DE MOTORES DE MOTOCICLETAS (CH: 100.0000)",
+    ambiente: "VTRIA-EXTER-EXTERNO",
+    chave: null,
+  },
+  {
+    id: 1326,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:30:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "HTC-MEC-4-92",
+    instrutor: "AFONSO DE JESUS CORDEIRO",
+    unidade_curricular:
+      "DESENVOLVIMENTO DE SISTEMAS DE AUTOMAÇÃO MECÂNICA (CH: 100.0000)",
+    ambiente: "VTRIA-3-LAB-3003",
+    chave: null,
+  },
+  {
+    id: 1300,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:00:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "EMP-NBM-03",
+    instrutor: "JOEL HERBERT BARBOSA SILVA",
+    unidade_curricular: "NOÇÕES BÁSICAS PARA MAQUINISTAS (CH: 219.0000)",
+    ambiente: "VTRIA-3-SALA-3004",
+    chave: null,
+  },
+  {
+    id: 1280,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:00:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "UMO-MBMM-03",
+    instrutor: "THADEU VASCONCELOS DA SILVA GOMES",
+    unidade_curricular:
+      "MECÂNICA BÁSICA DE MOTORES DE MOTOCICLETAS (CH: 100.0000)",
+    ambiente: "VTRIA-EXTER-EXTERNO",
+    chave: null,
+  },
+  {
+    id: 1326,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:30:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "HTC-MEC-4-92",
+    instrutor: "AFONSO DE JESUS CORDEIRO",
+    unidade_curricular:
+      "DESENVOLVIMENTO DE SISTEMAS DE AUTOMAÇÃO MECÂNICA (CH: 100.0000)",
+    ambiente: "VTRIA-3-LAB-3003",
+    chave: null,
+  },
+  {
+    id: 1300,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:00:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "EMP-NBM-03",
+    instrutor: "JOEL HERBERT BARBOSA SILVA",
+    unidade_curricular: "NOÇÕES BÁSICAS PARA MAQUINISTAS (CH: 219.0000)",
+    ambiente: "VTRIA-3-SALA-3004",
+    chave: null,
+  },
+  {
+    id: 1280,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:00:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "UMO-MBMM-03",
+    instrutor: "THADEU VASCONCELOS DA SILVA GOMES",
+    unidade_curricular:
+      "MECÂNICA BÁSICA DE MOTORES DE MOTOCICLETAS (CH: 100.0000)",
+    ambiente: "VTRIA-EXTER-EXTERNO",
+    chave: null,
+  },
+  {
+    id: 1326,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:30:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "HTC-MEC-4-92",
+    instrutor: "AFONSO DE JESUS CORDEIRO",
+    unidade_curricular:
+      "DESENVOLVIMENTO DE SISTEMAS DE AUTOMAÇÃO MECÂNICA (CH: 100.0000)",
+    ambiente: "VTRIA-3-LAB-3003",
+    chave: null,
+  },
+  {
+    id: 1300,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:00:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "EMP-NBM-03",
+    instrutor: "JOEL HERBERT BARBOSA SILVA",
+    unidade_curricular: "NOÇÕES BÁSICAS PARA MAQUINISTAS (CH: 219.0000)",
+    ambiente: "VTRIA-3-SALA-3004",
+    chave: null,
+  },
+  {
+    id: 1280,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:00:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "UMO-MBMM-03",
+    instrutor: "THADEU VASCONCELOS DA SILVA GOMES",
+    unidade_curricular:
+      "MECÂNICA BÁSICA DE MOTORES DE MOTOCICLETAS (CH: 100.0000)",
+    ambiente: "VTRIA-EXTER-EXTERNO",
+    chave: null,
+  },
+  {
+    id: 1326,
+    data: "2024-08-29T03:00:00.000Z",
+    data_hora_inicio: "2024-08-29T21:30:00.000Z",
+    data_hora_fim: "2024-08-30T01:00:00.000Z",
+    turma: "HTC-MEC-4-92",
+    instrutor: "AFONSO DE JESUS CORDEIRO",
+    unidade_curricular:
+      "DESENVOLVIMENTO DE SISTEMAS DE AUTOMAÇÃO MECÂNICA (CH: 100.0000)",
+    ambiente: "VTRIA-3-LAB-3003",
+    chave: null,
+  },
+];
 
-let tabela = document.getElementById('tabela');
+let tabela = document.getElementById("tabela");
 
-let exibir = '';
-for (let aula of aulas) {
-    exibir += '<tr>'
-    // exibir += '<td>' + aula.data + '</td>'
-    exibir += '<td>' + aula.data_hora_inicio + '</td>'
-    exibir += '<td>' + aula.data_hora_fim + '</td>'
-    exibir += '<td>' + aula.turma + '</td>'
-    exibir += '<td>' + aula.instrutor + '</td>'
-    exibir += '<td>' + aula.unidade_curricular + '</td>'
-    exibir += '<td>' + aula.ambiente + '</td>'
+let exibir = "";
+for (let i = 0; i < aulas.length; i++) {
+  let aula = aulas[i];
+  let dataInicio = new Date(aula.data_hora_inicio);
+  let dataFim = new Date(aula.data_hora_fim);
 
+  let horaInicio = dataInicio.toLocaleTimeString("pt-BR", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  let horaFim = dataFim.toLocaleTimeString("pt-BR", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 
+  let unidadeCurricular = aula.unidade_curricular;
+  let palavras = unidadeCurricular.split(" ");
+  let abreviado = palavras[0] + "." + palavras[palavras.length - 3];
 
-    exibir += '</tr>'
+  let classe = i % 2 === 0 ? "azul-claro" : "azul-escuro";
+
+  exibir += "<tr class='" + classe + "'>";
+  exibir += "<td>" + horaInicio + "</td>";
+  exibir += "<td>" + horaFim + "</td>";
+  exibir += "<td>" + aula.turma + "</td>";
+  exibir += "<td>" + aula.instrutor + "</td>";
+  exibir += "<td>" + abreviado + "</td>";
+  exibir += "<td>" + aula.ambiente + "</td>";
+  exibir += "</tr>";
 }
 
-
-// for (let i = 0; i < aulas.lenght; i++){
-//     let pieces = aulas[i].split(' '); //split pega uma string e separa em arrays de string
-//     pieces.splice(-2,2);//splice remove elemento de um array
-
-//     let abrev = pieces[0].substring(0,4);//retorna um pedaço de uma string
-//     console.log(abrev+'. '+pieces.pop());
-// }
-
-tabela.innerHTML += exibir;
-
-// console.log(tabela);
+document.getElementById("tabela").innerHTML += exibir;
